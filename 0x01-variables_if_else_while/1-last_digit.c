83f2d71324a08c2b0c885554528af9b90894c580
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-09-08 17:42:14 -0700

    Third commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,11 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+/**
+ * main - prints n value
+ *
+ * Return:  0 if success
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -13,11 +18,12 @@ int main(void)
 	/* your code goes there */
 	a = n % 10;
 	if (a > 5)
-		printf("%d and is greater than 5\n", a);
+		printf("%d and is greater than 5", a);
 	else if (a == 0)
 		printf("%d and is 0", a);
 	else
 		printf("%d and is less then 6 and not 0", a);
+	printf("\n");
 
 	return (0);
 }
