c182334fc5b6102915066c7192a48d0e77b896db
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-09-14 05:30:40 -0700

    first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-#include <stdio.h>
-/**
- * main - returns valule of n
- *
- * Return: 0 is success
- */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -15,12 +10,5 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if  (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
 	return (0);
 }
