d8c546b3d600bf475ea1d86f04e31a87adaa9cda
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-09-14 16:10:29 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int a = 1;
 
-	while (a <= 100)
+	while (a < 100)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
+		if (a % 5 == 0 && a % 3 == 0)
 			printf("FizzBuzz");
 		else if (a % 5 == 0)
 			printf("Buzz");
@@ -18,12 +18,8 @@ int main(void)
 			printf("Fizz");
 		else
 			printf("%d", a);
-		if (a < 100)
-			printf(" ");
-		else
-			printf("\n");
 		a++;
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is the start of c programming language. With is project we are expected to understand the fundermentals of c and the betty coding style.
+This is c programming handling Variables, if, else, while (loops and conditional statements
