bba767464ab62e1357df181abca3333e110c3255
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-09-29 11:40:51 -0700

    first commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main -  multiplies two numbers
+ * main -  prints all args
  * @argc: arguement count
  * @argv: array of pointers to CLI arg
+ *
  * Return: 0
  */
 int main(int argc, char *argv[])
@@ -12,14 +13,10 @@ int main(int argc, char *argv[])
 	int mul;
 
 	if (argc < 3)
-	{
 		printf("%s\n", "Error\n");
 		return (1);
-	}
 	else
-	{
 		 mul = atoi(argv[argc - 1]) * atoi(argv[argc - 2]);
 		 printf("%d\n", mul);
-	}
 	return (0);
 }
